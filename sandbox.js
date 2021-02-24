5b88cba9f8fbdb5b820f50e172338ec05221a701
zwf <zwf19980423@gmail.com>
2021-02-24 22:27:10 +0800

    ts compiler

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = 'hehe';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const character = 'hehe'
+console.log(character);
+
+const inputs = document.querySelectorAll('input')
+
+console.log(inputs);
+
+inputs.forEach(input => { 
+  console.log(input);
+  
+})
